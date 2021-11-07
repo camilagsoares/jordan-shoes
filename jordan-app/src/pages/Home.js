27c051f3ba07d884b9ebd4f013d302ca3e1c636d
camilagsoares <camila.soares19@hotmail.com>
2021-11-07 16:03:13 -0300

    Estrutura inicial feita

---
+++ jordan-app/src/pages/Home.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { BlackBelt } from '../styles/styled'
+import { WhiteBelt } from '../styles/styled'
+import { ContainerPhotoPrincipal } from '../styles/styled'
+import { ContainerHeader } from '../styles/styled'
+import { Footer } from '../styles/styled'
+import { CardProducts } from '../styles/styled'
+import { GridProducts } from '../styles/styled'
+import photoPrincipal from '../img/foto.jpeg'
+import shoesOne from '../img/1.png'
+import shoesTwo from '../img/2.png'
+import shoesThree from '../img/3.png'
+import shoesFour from '../img/4.png'
+import shoesFive from '../img/5.png'
+import shoesSix from '../img/6.png'
+import shoesSeven from '../img/7.png'
+import shoesEight from '../img/8.png'
+
+
+const Home = () => {
+    return(
+        <div>
+            <BlackBelt>
+                <p>Ganhe R$10,00 de desconto no frete</p>
+            </BlackBelt>
+         
+            <WhiteBelt>      
+                <h1>JordanShoes</h1>
+            </WhiteBelt>
+
+            <ContainerPhotoPrincipal>
+             <img src={photoPrincipal} />
+            </ContainerPhotoPrincipal>
+
+            <h1 className="destaques">Destaques</h1>
+            <p>Frete grátis e chinelo de brinde é aqui,aproveite por tempo limitado.</p>
+
+            <GridProducts>
+
+            <CardProducts>
+            <img src={shoesOne}/>
+            </CardProducts>
+
+            <CardProducts>
+            <img src={shoesTwo}/>
+            </CardProducts>
+
+            <CardProducts>
+            <img src={shoesThree}/>
+            </CardProducts>
+
+            <CardProducts>
+            <img src={shoesFour}/>
+            </CardProducts>
+            
+            <CardProducts>
+            <img src={shoesFive}/>
+            </CardProducts>
+
+            <CardProducts>
+            <img src={shoesSix}/>
+            </CardProducts>
+
+            <CardProducts>
+            <img src={shoesSeven}/>
+            </CardProducts>
+
+            <CardProducts>
+            <img src={shoesEight}/>
+            </CardProducts>
+            
+            </GridProducts>
+            <Footer>
+                <p>Todos os direitos reservados.</p>
+            </Footer>
+        </div>
+    )
+}
+
+export default Home
+
+   {/*
+
+            <ContainerHeader>
+            <p>O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan.</p>
+            </ContainerHeader> */}
+            // <h1 className="title">A melhor loja de Jordan</h1>
